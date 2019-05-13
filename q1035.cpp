2aa9687f28af330431818595be0a697a33fe7358
fanhaoyang002 <fanhaoyang002@gmail.com>
2019-05-12 21:00:47 -0700

    Update q1035.cpp
    format changes

--- q1035.cpp
+++ q1035.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-  int maxUncrossedLines(vector<int>& A, vector<int>& B) {
-    int m = A.size();
-    int n = B.size();
-    if (m == 0 || n == 0) return 0;
-    vector<int> temp;
-    temp.resize(n+1,0);
-    vector<vector<int>> matrix;
-    matrix.resize(m+1,temp);
-    for (int i = 0; i < m; ++i) {
-      for (int j = 0; j < n; ++j) {
-	if (A[i] == B[j]) matrix[i+1][j+1] = 1;
+  int maxUncrossedLines(const vector<int>& A, const vector<int>& B) {
+    // use const whenever possible
+    // use size_t
+    const size_t m = A.size();
+    const size_t n = B.size();
+    if (m == 0 || n == 0) {
+      return 0;
+    }
+	  
+    vector<vector<int>> matrix(m + 1, vector<int>(n + 1, 0));
+    for (size_t i = 0; i < m; ++i) {
+      for (size_t j = 0; j < n; ++j) {
+	if (A[i] == B[j]) {
+	  matrix[i + 1][j + 1] = 1;
+	}
       }
     }
 
-    for (int i = 1; i < m+1; ++i) {
-      for (int j = 1; j < n+1; ++j) {
-	matrix[i][j] = max(max(matrix[i-1][j-1]+matrix[i][j],matrix[i-1][j]),matrix[i][j-1]);
+    for (size_t i = 1; i < m + 1; ++i) {
+      for (size_t j = 1; j < n + 1; ++j) {
+	matrix[i][j] = max(max(matrix[i - 1][j - 1] + matrix[i][j], matrix[i - 1][j]), matrix[i][j - 1]);
       }
     }
     
