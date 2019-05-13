4d9a69e10ccf96f8f4b000f77afd473eac505d28
fanhaoyang002 <fanhaoyang002@gmail.com>
2019-05-12 21:07:52 -0700

    Update q11.cpp

--- q11.cpp
+++ q11.cpp
@@ -4,19 +4,22 @@
 
 class Solution {
 public:
-  int maxArea(vector<int>& height) {
-    int size = height.size();
-    if (size == 0) return 0;
+  int maxArea(const vector<int>& height) {
+    if (!height.empty()) {
+      return 0;
+    }
     int max_area = 0;
     int l = 0;
-    int r = size-1;
+    int r = height.size() - 1;
     while (l < r) {
       // calculate cur_area;
       max_area = max(max_area, (r-l)*min(height[l],height[r]));
-      if (height[l] < height[r]) l++;
-      else r--;
+      if (height[l] < height[r]) {
+        ++l;
+      } else {
+        r--;
+      }
     }
-
     return max_area;
   }
 };
